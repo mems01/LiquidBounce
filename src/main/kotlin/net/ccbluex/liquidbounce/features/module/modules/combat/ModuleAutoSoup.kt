c6288e5f562561569073421f05eb444c509fba76
memsss <aggelos.bilai73@gmail.com>
2021-07-21 21:11:30 +0300

    autosoup

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleAutoSoup.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleAutoSoup.kt
@@ -7,10 +7,7 @@ import net.ccbluex.liquidbounce.utils.item.convertClientSlotToServerSlot
 import net.minecraft.client.gui.screen.ingame.InventoryScreen
 import net.minecraft.item.Items
 import net.minecraft.item.MushroomStewItem
-import net.minecraft.network.packet.c2s.play.ClientCommandC2SPacket
-import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket
-import net.minecraft.network.packet.c2s.play.PlayerActionC2SPacket
-import net.minecraft.network.packet.c2s.play.PlayerInteractItemC2SPacket
+import net.minecraft.network.packet.c2s.play.*
 import net.minecraft.screen.slot.SlotActionType
 import net.minecraft.util.Hand
 import net.minecraft.util.math.BlockPos
@@ -20,15 +17,13 @@ object ModuleAutoSoup : Module("AutoSoup", Category.COMBAT) {
 
     val health by int("Health", 18, 1..20)
 
-    var lastSlot = -1
-
     val repeatable = repeatable {
         val hotBarSlot = (0..8).firstOrNull {
-            player.inventory.getStack(it).item is MushroomStewItem
+            player.inventory.getStack(it).item == Items.MUSHROOM_STEW
         }
 
         val invSlot = (0..40).find {
-            !player.inventory.isEmpty && player.inventory.getStack(it).item is MushroomStewItem
+            !player.inventory.isEmpty && player.inventory.getStack(it).item == Items.MUSHROOM_STEW
         }
 
         if (hotBarSlot == null && invSlot == null) {
@@ -41,23 +36,19 @@ object ModuleAutoSoup : Module("AutoSoup", Category.COMBAT) {
 
         if (player.health < health) {
             if (hotBarSlot != null) {
-                lastSlot = player.inventory.selectedSlot
-                player.inventory.selectedSlot = hotBarSlot
+                if (hotBarSlot != player.inventory.selectedSlot) {
+                    network.sendPacket(UpdateSelectedSlotC2SPacket(hotBarSlot))
+                }
 
                 network.sendPacket(PlayerInteractItemC2SPacket(Hand.MAIN_HAND))
 
-                if (player.inventory.getStack(hotBarSlot).item !is MushroomStewItem) {
-                    network.sendPacket(
-                        PlayerActionC2SPacket(
-                            PlayerActionC2SPacket.Action.DROP_ITEM,
-                            BlockPos.ORIGIN,
-                            Direction.DOWN
-                        )
-                    )
-                }
+                if (player.inventory.getStack(hotBarSlot).item != Items.MUSHROOM_STEW) {
+                    network.sendPacket(PlayerActionC2SPacket(PlayerActionC2SPacket.Action.DROP_ITEM, BlockPos.ORIGIN, Direction.DOWN))
 
-                player.inventory.selectedSlot = lastSlot
-                lastSlot = -1
+                    if (hotBarSlot != player.inventory.selectedSlot) {
+                        network.sendPacket(UpdateSelectedSlotC2SPacket(player.inventory.selectedSlot))
+                    }
+                }
             } else {
                 val serverSlot = convertClientSlotToServerSlot(invSlot!!)
 
