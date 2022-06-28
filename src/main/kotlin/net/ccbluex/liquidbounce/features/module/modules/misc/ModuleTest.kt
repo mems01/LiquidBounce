610dbbcdea40eace81fc8a6cdd342235e8d88dcb
mems01 <70612656+mems01@users.noreply.github.com>
2022-06-28 19:25:08 +0300

    test

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleTest.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/misc/ModuleTest.kt
@@ -5,26 +5,18 @@ import net.ccbluex.liquidbounce.event.TransferOrigin
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.utils.client.chat
-import net.minecraft.network.packet.s2c.play.BlockUpdateS2CPacket
-import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket
-import net.minecraft.network.packet.s2c.play.ChunkDeltaUpdateS2CPacket
-import net.minecraft.network.packet.s2c.play.ChunkRenderDistanceCenterS2CPacket
-import net.minecraft.network.packet.s2c.play.CommandSuggestionsS2CPacket
-import net.minecraft.network.packet.s2c.play.EntityTrackerUpdateS2CPacket
-import net.minecraft.network.packet.s2c.play.LightUpdateS2CPacket
-import net.minecraft.network.packet.s2c.play.UnloadChunkS2CPacket
-import net.minecraft.network.packet.s2c.play.WorldTimeUpdateS2CPacket
+import net.minecraft.network.packet.c2s.play.KeepAliveC2SPacket
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket
 
 object ModuleTest : net.ccbluex.liquidbounce.features.module.Module("Test", Category.MISC) {
     val packets by boolean("Packets", false)
 
     val packetHandler = handler<PacketEvent> { event ->
-        if (event.origin != TransferOrigin.RECEIVE || !packets) {
+        if (event.origin != TransferOrigin.SEND || !packets) {
             return@handler
         }
 
-        if (event.packet is ChunkDeltaUpdateS2CPacket || event.packet is WorldTimeUpdateS2CPacket || event.packet is ChunkDataS2CPacket || event.packet is UnloadChunkS2CPacket || event.packet is EntityTrackerUpdateS2CPacket || event.packet is LightUpdateS2CPacket || event.packet is CommandSuggestionsS2CPacket || event.packet is ChunkRenderDistanceCenterS2CPacket || event.packet is BlockUpdateS2CPacket) {
-            event.cancelEvent()
+        if (event.packet is PlayerMoveC2SPacket || event.packet is KeepAliveC2SPacket) {
             return@handler
         }
 
