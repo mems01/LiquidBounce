25f4a84718f4a97225b326d8ba354d4baed08dbc
mems01 <70612656+mems01@users.noreply.github.com>
2022-12-03 18:14:21 +0200

    tests

--- src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleSuperKnockback.kt
+++ src/main/kotlin/net/ccbluex/liquidbounce/features/module/modules/combat/ModuleSuperKnockback.kt
@@ -20,12 +20,13 @@
 package net.ccbluex.liquidbounce.features.module.modules.combat
 
 import net.ccbluex.liquidbounce.event.AttackEvent
-import net.ccbluex.liquidbounce.event.PostAttackEvent
+import net.ccbluex.liquidbounce.event.PacketEvent
 import net.ccbluex.liquidbounce.event.handler
 import net.ccbluex.liquidbounce.event.sequenceHandler
 import net.ccbluex.liquidbounce.features.module.Category
 import net.ccbluex.liquidbounce.features.module.Module
 import net.minecraft.client.util.InputUtil
+import net.minecraft.network.packet.c2s.play.PlayerInteractEntityC2SPacket
 
 /**
  * SuperKnockback module
@@ -34,16 +35,24 @@ import net.minecraft.client.util.InputUtil
  */
 object ModuleSuperKnockback : Module("SuperKnockback", Category.COMBAT) {
 
+    val pressingSprintKey: Boolean
+        get() = InputUtil.isKeyPressed(mc.window.handle, mc.options.sprintKey.boundKey.code)
+
     val attackHandler = handler<AttackEvent> {
-        if (player.isSprinting && InputUtil.isKeyPressed(mc.window.handle, mc.options.sprintKey.boundKey.code)) {
+        if (player.isSprinting && pressingSprintKey) {
             mc.options.sprintKey.isPressed = false
         }
     }
 
-    val postAttackHandler = sequenceHandler<PostAttackEvent> {
-        waitUntil { player.isSprinting == player.lastSprinting }
+    val postAttackHandler = sequenceHandler<PacketEvent> {
+        // type.type typical mojang
+        if (it.packet !is PlayerInteractEntityC2SPacket || it.packet.type.type != PlayerInteractEntityC2SPacket.InteractType.ATTACK) {
+            return@sequenceHandler
+        }
 
-        if (!player.isSprinting && InputUtil.isKeyPressed(mc.window.handle, mc.options.sprintKey.boundKey.code)) {
+        waitUntil { !player.isSprinting && player.isSprinting == player.lastSprinting }
+
+        if (!mc.options.sprintKey.isPressed && pressingSprintKey) {
             mc.options.sprintKey.isPressed = true
         }
     }
